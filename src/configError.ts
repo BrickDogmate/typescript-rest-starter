661abb32b1cd8fe635a6f87c669943c6afeef047
Brick <brick@dogmate.co.kr>
2020-03-16 00:59:01 +0900

    404 에러 처리 오류 수정

--- src/configError.ts
+++ src/configError.ts
@@ -2,27 +2,32 @@ import * as express from 'express';
 import { ERROR_MESSAGE } from './common/ErrorMessages';
 
 export function configError(app: express.Application): void {
-  app.use(error404);
   app.use(errorJsonResponse);
+  app.use(error404);
 }
 
-function error404(req: express.Request, res: express.Response): void {
+function error404(req: express.Request, res: express.Response, next: express.NextFunction): void {
+  if (res.headersSent) return next();
+
+  res.set('Content-Type', 'application/json');
   res.status(404).json({ error: ERROR_MESSAGE.NOT_FOUND_PATH });
 }
 
 function errorJsonResponse(err: any, req: express.Request, res: express.Response, next: express.NextFunction): void {
-  if (res.headersSent) {
-    return next();
-  }
+  if (res.headersSent) return next();
 
-  if (process.env.NODE_ENV !== 'test') {
-    console.error(err.stack);
-  }
-
-  res.set('Content-Type', 'application/json');
-  if (!err.statusCode) {
-    res.status(500).json({ error: ERROR_MESSAGE.SYSTEM_ERROR });
+  if (err) {
+    if (process.env.NODE_ENV !== 'test') {
+      console.error(err);
+      console.error(err.stack);
+    }
+    res.set('Content-Type', 'application/json');
+    if (!err.statusCode) {
+      res.status(500).json({ error: ERROR_MESSAGE.SYSTEM_ERROR });
+    } else {
+      res.status(err.statusCode).json({ error : err.message });
+    }
   } else {
-    res.status(err.statusCode).json({ error : err.message });
+    next();
   }
 }
